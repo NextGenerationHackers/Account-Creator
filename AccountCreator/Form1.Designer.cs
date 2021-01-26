d0307a625f593f5d8242c17cba69eaafddd365fb
Julian <Julianty123@users.noreply.github.com>
2021-01-26 11:31:27 -0500

    Agregar archivos de proyecto.

---
+++ AccountCreator/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace AccountCreator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelPosition = new System.Windows.Forms.Label();
+            this.labelResolution = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.labelNumber = new System.Windows.Forms.Label();
+            this.labelInformation = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Ravie", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(361, 172);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(196, 137);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start bot";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labelPosition
+            // 
+            this.labelPosition.AutoSize = true;
+            this.labelPosition.ForeColor = System.Drawing.SystemColors.Control;
+            this.labelPosition.Location = new System.Drawing.Point(375, 379);
+            this.labelPosition.Name = "labelPosition";
+            this.labelPosition.Size = new System.Drawing.Size(167, 17);
+            this.labelPosition.TabIndex = 5;
+            this.labelPosition.Text = "Mouse position: (0, 0)";
+            // 
+            // labelResolution
+            // 
+            this.labelResolution.AutoSize = true;
+            this.labelResolution.ForeColor = System.Drawing.SystemColors.Control;
+            this.labelResolution.Location = new System.Drawing.Point(12, 9);
+            this.labelResolution.Name = "labelResolution";
+            this.labelResolution.Size = new System.Drawing.Size(90, 17);
+            this.labelResolution.TabIndex = 6;
+            this.labelResolution.Text = "Resolution:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(15, 32);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(322, 410);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 400;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(392, 59);
+            this.hScrollBar1.Maximum = 109;
+            this.hScrollBar1.Minimum = 1;
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(106, 20);
+            this.hScrollBar1.TabIndex = 8;
+            this.hScrollBar1.Value = 1;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
+            // 
+            // labelNumber
+            // 
+            this.labelNumber.AutoSize = true;
+            this.labelNumber.ForeColor = System.Drawing.SystemColors.Control;
+            this.labelNumber.Location = new System.Drawing.Point(375, 32);
+            this.labelNumber.Name = "labelNumber";
+            this.labelNumber.Size = new System.Drawing.Size(172, 17);
+            this.labelNumber.TabIndex = 9;
+            this.labelNumber.Text = "Number of accounts: 1";
+            // 
+            // labelInformation
+            // 
+            this.labelInformation.AutoSize = true;
+            this.labelInformation.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic)
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelInformation.ForeColor = System.Drawing.SystemColors.Control;
+            this.labelInformation.Location = new System.Drawing.Point(12, 445);
+            this.labelInformation.Name = "labelInformation";
+            this.labelInformation.Size = new System.Drawing.Size(0, 17);
+            this.labelInformation.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(39)))));
+            this.ClientSize = new System.Drawing.Size(601, 471);
+            this.Controls.Add(this.labelInformation);
+            this.Controls.Add(this.labelNumber);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.labelResolution);
+            this.Controls.Add(this.labelPosition);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Account Creator";
+            this.TopMost = true;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelPosition;
+        private System.Windows.Forms.Label labelResolution;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.Label labelNumber;
+        private System.Windows.Forms.Label labelInformation;
+    }
+}
+
---
+++ AccountCreator/Form1.cs
@@ -0,0 +1,224 @@
+﻿using PuppeteerSharp;
+using PuppeteerSharp.Input;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsInput;
+
+namespace AccountCreator
+{
+    public partial class Form1 : Form
+    {
+        /* 
+            https://csharp.hotexamples.com/es/examples/WindowsInput/KeyboardSimulator/-/php-keyboardsimulator-class-examples.html
+            https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.sendkeys.send?view=net-5.0
+
+            http://www.puppeteersharp.com/api/index.html
+            https://www.kiltandcode.com/puppeteer-sharp-crawl-the-web-using-csharp-and-headless-chrome/
+        */
+
+        InputSimulator inputSimulator = new InputSimulator();
+        public int delay = 100;
+        public int Count;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            string screenWidth = Screen.PrimaryScreen.Bounds.Width.ToString();
+            string screenHeight = Screen.PrimaryScreen.Bounds.Height.ToString();
+            labelResolution.Text = ("Resolution: " + screenWidth + "x" + screenHeight);
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            string url = "https://accounts.google.com/signup/v2/webcreateaccount?service=mail&continue=https%3A%2F%2Fmail.google.com"
+            + "%2Fmail&hl=es&dsh=S1912622997%3A1611341957592098&gmb=exp&biz=false&flowName=GlifWebSignIn&flowEntry=SignUp";
+
+            richTextBox1.Text = string.Empty;
+            labelInformation.Text = string.Empty;
+            button1.Enabled = false;
+            timer2.Start();
+            button1.Text = "Downloading the chromium version this may take a while...";
+            await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
+            button1.Text = "Running chromium instance...";
+            var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+            {
+                Headless = false,
+                DefaultViewport = null
+            });
+
+            button1.Text = "Working...";
+            var page = await browser.NewPageAsync();
+            await page.GoToAsync(url);
+            await page.BringToFrontAsync();
+
+            // Ignore this line: var six = await page.EvaluateFunctionAsync<int>("async () => await Promise.resolve(6)");
+
+            // This "loop" is executed until the number of accounts is completed
+            while (hScrollBar1.Value > Count)
+            {
+                await CreateAccount(page, delay);
+                if (hScrollBar1.Value > Count)
+                {
+                    await page.ReloadAsync(); // Reload the page if there are more accounts to create
+                }
+            }
+
+            timer2.Stop(); // Stops the timer of "animation"
+
+            await browser.CloseAsync(); // Close chromium (browser) and all the pages in it
+            button1.Text = "Automation finished.";
+            await Task.Delay(1000);
+            button1.Text = "Automation finished..";
+            await Task.Delay(1000);
+            button1.Text = "Automation finished...";
+            await Task.Delay(1000);
+
+            // Copy the text from the richtextbox to a notepad to save it on your computer
+            StreamWriter writer = new StreamWriter("C:\\Users\\" + Environment.UserName + "\\Desktop\\Accounts from scripting.txt");
+            foreach (object line in richTextBox1.Lines)
+            {
+                writer.WriteLine(line);
+            }
+            writer.Close();
+
+            labelInformation.Text = "The account information has been saved to the desktop.";
+            Count = 0;
+            button1.Text = "Start bot";
+            button1.Enabled = true;
+        }
+
+        private async Task CreateAccount(Page page, int delay)
+        {
+            // http://www.asciitable.com/
+            Random random = new Random();
+
+            var firstNameSelector = "#firstName";
+            await page.WaitForSelectorAsync(firstNameSelector);
+            int Number1 = random.Next(65, 91);
+            var Letter1 = Convert.ToChar(Number1).ToString();
+            int Number2 = random.Next(65, 91);
+            var Letter2 = Convert.ToChar(Number2).ToString();
+            int Number3 = random.Next(65, 91);
+            var Letter3 = Convert.ToChar(Number3).ToString();
+            int Number4 = random.Next(65, 91);
+            var Letter4 = Convert.ToChar(Number4).ToString();
+            int Number5 = random.Next(65, 91);
+            var Letter5 = Convert.ToChar(Number5).ToString();
+            // Name
+            string Name = Letter1 + Letter2 + Letter3 + Letter4 + Letter5;
+            await TypeFieldValue(page, firstNameSelector, Name, delay); // Run this method with the parameters
+
+            var lastNameSelector = "#lastName";
+            //await page.Keyboard.PressAsync("Tab");
+            int Number6 = random.Next(65, 91);
+            var Letter6 = Convert.ToChar(Number6).ToString();
+            int Number7 = random.Next(65, 91);
+            var Letter7 = Convert.ToChar(Number7).ToString();
+            int Number8 = random.Next(65, 91);
+            var Letter8 = Convert.ToChar(Number8).ToString();
+            int Number9 = random.Next(65, 91);
+            var Letter9 = Convert.ToChar(Number9).ToString();
+            int Number10 = random.Next(65, 91);
+            var Letter10 = Convert.ToChar(Number10).ToString();
+            // Last name
+            string LastName = Letter6 + Letter7 + Letter8 + Letter9 + Letter10;
+            await TypeFieldValue(page, lastNameSelector, LastName, delay);
+
+            var UserName = "#username";
+            // The mail might not be available sometimes, i need to fix that
+            string Mail = (Letter2 + Letter4 + Letter5 + Letter6 + Letter8 + Letter9 + Letter10).ToLower();
+            await TypeFieldValue(page, UserName, Mail, delay);
+            await Task.Delay(delay);
+            SendKeys.Send("{TAB}");
+            await Task.Delay(delay);
+            // Password
+            string password = Letter1 + Letter3 + Letter5 + Letter7 + Letter9 + "123";
+            inputSimulator.Keyboard.TextEntry(password);
+            await Task.Delay(delay);
+            SendKeys.Send("{TAB}");
+            await Task.Delay(delay);
+            inputSimulator.Keyboard.TextEntry(password);
+            await Task.Delay(delay);
+            await page.ClickAsync(".VfPpkd-RLmnJb");    // Press the button
+            await Task.Delay(delay * 30);
+            Count++;
+            richTextBox1.Text += "Gmail Account ~ " + Count + Environment.NewLine;
+            richTextBox1.Text += "Name: " + Name + "    " + "LastName: " + LastName + Environment.NewLine;
+            richTextBox1.Text += "Mail: " + Mail + "@gmail.com" + "    " + "Password: " + password + Environment.NewLine;
+            richTextBox1.Text += Environment.NewLine;
+            await Task.Delay(delay);
+            // I need to configure the phone number looool
+        }
+
+        // It gives the impression that a real person is writing
+        private static async Task TypeFieldValue(Page page, string fieldSelector, string value, int delay = 0)
+        {
+            await page.FocusAsync(fieldSelector);
+            await page.TypeAsync(fieldSelector, value, new TypeOptions { Delay = delay });
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // In my case i can't get the true resolution of the monitor, i dont know why
+            labelPosition.Text = "Mouse position: (" + Cursor.Position.X + ", " + Cursor.Position.Y + ")";
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            // This "animates" the label, lmfao (I dont know how to say that)
+            if (button1.Text == "Downloading the chromium version this may take a while...")
+            {
+                button1.Text = "Downloading the chromium version this may take a while.";
+            }
+            else if (button1.Text == "Downloading the chromium version this may take a while.")
+            {
+                button1.Text = "Downloading the chromium version this may take a while..";
+            }
+            else if (button1.Text == "Downloading the chromium version this may take a while..")
+            {
+                button1.Text = "Downloading the chromium version this may take a while...";
+            }
+            else if (button1.Text == "Running chromium instance...")
+            {
+                button1.Text = "Running chromium instance.";
+            }
+            else if (button1.Text == "Running chromium instance.")
+            {
+                button1.Text = "Running chromium instance..";
+            }
+            else if (button1.Text == "Running chromium instance..")
+            {
+                button1.Text = "Running chromium instance...";
+            }
+            else if (button1.Text == "Working...")
+            {
+                button1.Text = "Working.";
+            }
+            else if (button1.Text == "Working.")
+            {
+                button1.Text = "Working..";
+            }
+            else if (button1.Text == "Working..")
+            {
+                button1.Text = "Working...";
+            }
+        }
+
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            labelNumber.Text = "Number of accounts: " + hScrollBar1.Value;
+        }
+    }
+}
